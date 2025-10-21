5a2cbe5a3556be740124060fce368afe1479d3bf
Incube700 <Belkajor@gmail.com>
2025-10-22 01:24:59 +0530

    try no2

--- Assets/Scripts/RollingBallMover.cs
+++ Assets/Scripts/RollingBallMover.cs
@@ -7,7 +7,7 @@ public class RollingBallMover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Привет");
+        Debug.Log("Привет проверка сохранений");
     }
 
     // Update is called once per frame
